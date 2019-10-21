538672b15c30b68a5d16a94b04d53d3d5b32efb9
Tyrel <tyrelclayton92@gmail.com>
2019-10-20 21:27:40 -0400

    fix instagram feed

--- src/components/public/__tests__/menu_item.spec.js
+++ src/components/public/__tests__/menu_item.spec.js
@@ -3,46 +3,23 @@ import { shallow } from "enzyme";
 import MenuItem from "../menu_item";
 
 describe("Menu", () => {
-  it("renders the price correctly", () => {
-    const mockItem = {
+  const mockProps = {
+    item: {
       name: "testItem",
       price: "40",
       description: "a test item, used for testing"
-    };
+    }
+  };
 
-    const wrapper = shallow(<MenuItem item={mockItem} />);
+  it.each`
+    enzymeid                    | expectedText
+    ${"menu.item.price"}        | ${"$40"}
+    ${"menu.item.nameAndPrice"} | ${"testItem$40"}
+    ${"menu.item.description"}  | ${"a test item, used for testing"}
+  `("renders the element correctly", ({ enzymeid, expectedText }) => {
+    const wrapper = shallow(<MenuItem {...mockProps} />);
 
-    expect(wrapper.find({ enzymeId: "menu.item.price" }).text()).toBe("$40");
-    wrapper.unmount();
-  });
-
-  it("renders the price correctly", () => {
-    const mockItem = {
-      name: "testItem",
-      price: "40",
-      description: "a test item, used for testing"
-    };
-
-    const wrapper = shallow(<MenuItem item={mockItem} />);
-
-    expect(wrapper.find({ enzymeId: "menu.item.nameAndPrice" }).text()).toBe(
-      "testItem$40"
-    );
-    wrapper.unmount();
-  });
-
-  it("renders the price correctly", () => {
-    const mockItem = {
-      name: "testItem",
-      price: "40",
-      description: "a test item, used for testing"
-    };
-
-    const wrapper = shallow(<MenuItem item={mockItem} />);
-
-    expect(wrapper.find({ enzymeId: "menu.item.description" }).text()).toBe(
-      "a test item, used for testing"
-    );
+    expect(wrapper.find({ enzymeid: enzymeid }).text()).toBe(expectedText);
     wrapper.unmount();
   });
 });
--- src/components/public/menu_item.js
+++ src/components/public/menu_item.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './MenuItem.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./MenuItem.css";
 
 const MenuItem = props => {
   return (
     <div className="container col-sm-6 col-md-4">
       <div className="menuItem p-2 m-1">
-        <p enzymeId="menu.item.nameAndPrice" className="font-weight-bold">
+        <p enzymeid="menu.item.nameAndPrice" className="font-weight-bold">
           {props.item.name}
-          <span enzymeId="menu.item.price" className="float-right">
+          <span enzymeid="menu.item.price" className="float-right">
             ${props.item.price}
           </span>
         </p>
-        <p enzymeId="menu.item.description" className="font-weight-light">
+        <p enzymeid="menu.item.description" className="font-weight-light">
           {props.item.description}
         </p>
       </div>
